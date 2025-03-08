854923564baea1dc51801506a45c9148743e42b1
tanvircr7 <tanvir.hasan.cr7@gmail.com>
2025-03-08 17:46:27 -0600

    color scheme

--- src/lib/theme.svelte.ts
+++ src/lib/theme.svelte.ts
@@ -4,7 +4,8 @@ class Theme {
 	current = $state(browser && localStorage.getItem('color-scheme'))
 
 	toggle = () => {
-		const theme = this.current === 'dark' ? 'light' : 'dark'
+		// const theme = this.current === 'dark' ? 'light' : 'dark'
+		const theme = this.current === 'dark' ? 'dark' : 'dark'
 		document.documentElement.setAttribute('color-scheme', theme)
 		localStorage.setItem('color-scheme', theme)
 		this.current = theme
